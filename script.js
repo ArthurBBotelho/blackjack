1e3705e94bd07d3fd99c30767b06afc33bacf554
arthur <emilioarthur19@gmail.com>
2022-05-17 21:36:12 -0300

    Uptated the code added a for loop inside the function so the player can see what new card is being played, the loop root is the index of the array, the array contains the cards

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
+
 let firstCard = 10
 let secondCard = 10
+let cards = [firstCard, secondCard]
+
 let sum = firstCard + secondCard
 
-let message
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-if (sum < 21) {
-    message = "You got " + sum + " witch is less than 21!"
-}
-else if (sum === 21) {
-    message = "Blackjack! You got 21!"
-}
-else {
-    message = "You're out of the game!"
+function startGame(){
+    renderGame()
 }
 
-console.log(message)
+function renderGame(){
+    cardsEl.textContent = "Cards: "
+    sumEl.textContent = "Sum: " + sum
+    
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += (cards[i]) + " | "
+    } 
+
+    if (sum < 21) {
+        message = "You got " + sum + " witch is less than 21!"
+    }
+    else if (sum === 21) {
+        message = "Blackjack! You got 21!"
+    }
+    else {
+        message = "You're out of the game!"
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    let newCard = 1
+    sum += newCard
+    cards.push(newCard)
+    startGame()
+}
